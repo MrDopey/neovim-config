d4ae512229f066aabc13f83118f1ac3f79ba2117
Gene Luong <beforewards@gmail.com>
2024-03-17 22:23:21 +1100

    feat(nvim): added yaml to tree-sitter

--- .config/nvim/lua/treesitter-setup.lua
+++ .config/nvim/lua/treesitter-setup.lua
@@ -4,7 +4,7 @@
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'bash', 'c', 'cpp', 'javascript', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+    ensure_installed = { 'bash', 'c', 'cpp', 'javascript', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'yaml' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
