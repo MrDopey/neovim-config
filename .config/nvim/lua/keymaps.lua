a5f56e4c8e35f114e8ebeb64a0671664dad03029
Gene Luong <beforewards@gmail.com>
2024-04-28 00:27:17 +1000

    feat(nvim): added keymap to swallow deletes

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -34,4 +34,11 @@ vim.keymap.set('n', '<C-u>', "<C-u>zz")
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv")
 
+-- next greatest remap ever : asbjornHaland
+-- vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+-- vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- swallow the delete so you don't mess with the default register
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
 -- vim: ts=2 sts=2 sw=2 et
