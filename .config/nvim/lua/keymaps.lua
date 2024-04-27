2304daa1ad179ef2fef6669f05cf1240e9d0cbd3
Gene Luong <beforewards@gmail.com>
2024-04-28 01:25:17 +1000

    feat(nvim): keep system clipboard independent and added keymap to interact with it easier

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -35,8 +35,8 @@ vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv")
 
 -- next greatest remap ever : asbjornHaland
--- vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
--- vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- swallow the delete so you don't mess with the default register
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
